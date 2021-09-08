3f6f8a9338e66236de4c3a3b360b0fb84ecce300
Scott Roche <scott.roche24@gmail.com>
2021-09-08 20:18:40 +0100

    Fixes missing function headers.

--- src/engine/renderer/framebuffer.h
+++ src/engine/renderer/framebuffer.h
@@ -7,17 +7,37 @@
 #include "devices.h"
 #include "graphics_pipeline.h"
 
+/******************************************************************************
+ * @name  _Framebuffer
+ * @brief A struct to hold a VkFramebuffer.
+******************************************************************************/
 struct _Framebuffer
 {
 	VkFramebuffer handle;
 };
 typedef struct _Framebuffer Framebuffer;
 
+/******************************************************************************
+ * @name      framebuffer_create()
+ * @brief     Creates a framebuffer.
+ * @param[in] device     The device that the framebuffer belongs to.
+ * @param[in] pipeline   The pipeline that will be used with the framebuffer.
+ * @param[in] image_view The image view that the framebuffer will display.
+ * @param[in] extent     The extent of the framebuffer.
+ * @return    A pointer to a framebuffer.
+******************************************************************************/
 Framebuffer *framebuffer_create(const Device *restrict device,
                                 const GraphicsPipeline *restrict pipeline,
                                 const VkImageView *restrict image_view,
                                 const VkExtent2D *restrict extent);
 
+/******************************************************************************
+ * @name      framebuffer_destroy()
+ * @brief     Destroys a specified framebuffer.
+ * @param[in] framebuffer The framebuffer to destroy.
+ * @param[in] device      The device for which the framebuffer belongs to.
+ * @return    void
+******************************************************************************/
 void framebuffer_destroy(Framebuffer *restrict framebuffer,
                          Device *restrict device);
 
